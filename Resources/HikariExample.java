9f880af778055a9b3d861b07356cc9974ab27992
CeramicTitan <ceramictitandev@gmail.com>
2016-04-13 02:03:30 -0600

    Added comments explaining what is happening.
    This is a good way to provide a reference as opposed to people having to go back to your video.

--- Resources/HikariExample.java
+++ Resources/HikariExample.java
@@ -8,21 +8,22 @@ import java.sql.SQLException;
 import java.util.UUID;
 
 public class HikariExample extends JavaPlugin {
-
+    //Create global Hikari instance
     private HikariDataSource hikari;
-
+    //On server start, we connect to our database
     @Override
     public void onEnable() {
         connectToDatabase();
     }
-
     public void connectToDatabase() {
+        //Database details
         String address = getConfig().getString("Database.Address");
         String name = getConfig().getString("Database.Name");
         String username = getConfig().getString("Database.Username");
         String password = getConfig().getString("Database.Password");
-
+        //Initialise hikari instace
         hikari = new HikariDataSource();
+        //Setting Hikari properties
         hikari.setMaximumPoolSize(10);
         hikari.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
         hikari.addDataSourceProperty("serverName", address);
@@ -33,24 +34,30 @@ public class HikariExample extends JavaPlugin {
     }
 
     public void runAsyncQuery(UUID playerUUID) {
+        //Create connection instance
         Connection connection = null;
-
+        //MySQL query: https://www3.ntu.edu.sg/home/ehchua/programming/sql/MySQL_Beginner.html
         String update = "INSERT INTO database VALUES(?, ?) ON DUPLICATE KEY UPDATE uuid=?";
 
         PreparedStatement p = null;
 
         try {
+            //Initialise hikari connection, by getting the hikari connect if established
             connection = hikari.getConnection();
-
+            //Preparing statement - INSERT INTO...
             p = connection.prepareStatement(update);
+            //Setting parameters in MySQL query: i.e the question marks(?), where the first one has the index of 1.
             p.setString(1, playerUUID.toString());
             p.setInt(2, 0);
+            //ON DUPLICATE UPDATE updates the current uuid, if it exists
             p.setString(3, playerUUID.toString());
+            //Executes the statement
             p.execute();
         } catch (SQLException e) {
+            //Print out any exception while trying to prepare statement
             e.printStackTrace();
         } finally {
-
+            //After catching the statement, close connection if connection is established
             if (connection != null) {
                 try {
                     connection.close();
@@ -59,7 +66,7 @@ public class HikariExample extends JavaPlugin {
                 }
             }
 
-            // Organize
+            // If connection is established, close connection after query
             if(p != null) {
                 try {
                     p.close();
